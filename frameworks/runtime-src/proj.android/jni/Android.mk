f45ba4198fe2c309f0052ea758f2b99801a2e3d4
Carl <13352840347@163.com>
2016-05-28 21:11:30 +0800

    cocos2d-js
    泡泡糖——基础

---
+++ frameworks/runtime-src/proj.android/jni/Android.mk
@@ -0,0 +1,30 @@
+LOCAL_PATH := $(call \
+my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := cocos2djs_shared
+
+LOCAL_MODULE_FILENAME := libcocos2djs
+
+LOCAL_SRC_FILES := hellojavascript/main.cpp \
+../../Classes/AppDelegate.cpp \
+../../Classes/jsb_anysdk_basic_conversions.cpp \
+../../Classes/manualanysdkbindings.cpp \
+../../Classes/jsb_anysdk_protocols_auto.cpp
+
+LOCAL_CPPFLAGS := -DSDKBOX_ENABLED
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_WHOLE_STATIC_LIBRARIES := PluginProtocolStatic
+
+LOCAL_STATIC_LIBRARIES := cocos2d_js_static
+
+LOCAL_EXPORT_CFLAGS := -DCOCOS2D_DEBUG=2 \
+-DCOCOS2D_JAVASCRIPT
+
+include $(BUILD_SHARED_LIBRARY)
+$(call import-add-path, $(LOCAL_PATH)/..)
+
+
+$(call import-module, scripting/js-bindings/proj.android)
+$(call import-module,protocols/android)
